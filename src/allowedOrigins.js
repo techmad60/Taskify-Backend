9b61f400640b7d70147546628b21723c3989a19a
Techmad <techmad60@gmail.com>
2024-10-07 05:49:18 -0700

    Power

--- src/allowedOrigins.js
+++ src/allowedOrigins.js
@@ -1,7 +1,7 @@
 // allowedOrigins.js
 
 const allowedOrigins = [
-    'http://localhost:3000', // Local development URL
+    'http://localhost:3001', // Local development URL
     'https://taskify-ten-hazel.vercel.app', // Production URL
 ];
 
